394658ed1c1a12e304cb04b67e753c6bae1f27af
bakatsuyuki <gurow5811>
2018-06-12 14:17:10 +0900

    Sample activity

--- app/src/main/java/jp/kuluna/eventgridview/sample/MainActivity.kt
+++ app/src/main/java/jp/kuluna/eventgridview/sample/MainActivity.kt
@@ -2,11 +2,46 @@ package jp.kuluna.eventgridview.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import jp.kuluna.eventgridview.Event
+import jp.kuluna.eventgridview.EventGridAdapter
+import jp.kuluna.eventgridview.EventGridView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var eventGridView: EventGridView
+    private lateinit var adapter: EventGridAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        eventGridView = findViewById(R.id.event_grid_view)
+        adapter = EventGridAdapter(this)
+        eventGridView.adapter = adapter
+        showEvents()
+    }
+
+    private fun showEvents() {
+        val startedAt = Date()
+        val endedAt = Calendar.getInstance().apply {
+            time = startedAt
+            add(Calendar.HOUR_OF_DAY, 12)
+        }.time
+        val gridColor = ContextCompat.getColor(this, android.R.color.holo_blue_light)
+        val blackColor = ContextCompat.getColor(this, android.R.color.black)
+
+        val events: List<Event> = ArrayList<Event>().apply {
+            add(Event(
+                    0,
+                    startedAt,
+                    endedAt,
+                    "名前",
+                    gridColor,
+                    blackColor,
+                    null,
+                    null))
+        }
+
+        adapter.replace(events, Date())
+        //eventGridView.setOverTime()
     }
 }
