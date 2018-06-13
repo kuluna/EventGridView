61541d1537811492c4390339ed801b1282922e61
bakatsuyuki <bakatsuyuki@icloud.com>
2018-06-13 12:49:22 +0900

    Over 24 hour

--- app/src/main/java/jp/kuluna/eventgridview/sample/MainActivity.kt
+++ app/src/main/java/jp/kuluna/eventgridview/sample/MainActivity.kt
@@ -3,6 +3,8 @@ package jp.kuluna.eventgridview.sample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.view.Menu
+import android.view.MenuItem
 import jp.kuluna.eventgridview.Event
 import jp.kuluna.eventgridview.EventGridAdapter
 import jp.kuluna.eventgridview.EventGridView
@@ -14,17 +16,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         eventGridView = findViewById(R.id.event_grid_view)
         adapter = EventGridAdapter(this)
         eventGridView.adapter = adapter
         showEvents()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.reloadButton -> {
+                showEvents()
+            }
+        }
+        return true
+    }
+
     private fun showEvents() {
         val startedAt = Date()
         val endedAt = Calendar.getInstance().apply {
             time = startedAt
-            add(Calendar.HOUR_OF_DAY, 12)
+            add(Calendar.HOUR_OF_DAY, Random().nextInt(24))
         }.time
         val gridColor = ContextCompat.getColor(this, android.R.color.holo_blue_light)
         val blackColor = ContextCompat.getColor(this, android.R.color.black)
