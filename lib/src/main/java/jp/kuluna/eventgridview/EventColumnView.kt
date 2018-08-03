5a8545fbdcd7f365d4075ec782d89ab838f96fb6
bakatsuyuki <gurow5811>
2018-08-03 18:04:31 +0900

    Width is match parent

--- lib/src/main/java/jp/kuluna/eventgridview/EventColumnView.kt
+++ lib/src/main/java/jp/kuluna/eventgridview/EventColumnView.kt
@@ -22,7 +22,7 @@ import kotlin.math.roundToInt
  * @param context Android Context
  */
 @SuppressLint("ViewConstructor")
-open class EventColumnView(context: Context,  widthIsMatchParent: Boolean) : FrameLayout(context) {
+open class EventColumnView(context: Context, widthIsMatchParent: Boolean) : FrameLayout(context) {
     /** Eventのクリックイベント */
     var onEventClickListener: ((Event) -> Unit)? = null
     /** Eventのドラッグイベント */
