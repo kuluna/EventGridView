99b15a81c717f2a8bae1829eb78e16dd9e4a9b29
bakatsuyuki <gurow5811>
2018-09-13 15:15:39 +0900

    Fix bag

--- lib/src/main/java/jp/kuluna/eventgridview/EventColumnView.kt
+++ lib/src/main/java/jp/kuluna/eventgridview/EventColumnView.kt
@@ -88,8 +88,9 @@ open class EventColumnView(context: Context, widthIsMatchParent: Boolean) : Fram
                     val eventBinding = DataBindingUtil.bind<ViewEventBinding>(dragEvent.localState as View)!!
 
                     oldEvent = event
-                    // EventGridView上部のマージン(20dp)分下にずれるので補正
-                    var dropStartY = dragEvent.y - adjustStartTapY - 20
+                    var dropStartY = dragEvent.y - adjustStartTapY
+                    // EventGridView上部のマージン分下にずれるので補正
+                    dropStartY -= aScale / 2 / density
                     // 開始地点がマイナスになった時は0時0分開始にする
                     if (dropStartY < 0) {
                         dropStartY = 0f
