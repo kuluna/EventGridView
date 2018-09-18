920910092a82b280263b5591234dd14909be2262
bakatsuyuki <gurow5811>
2018-09-18 13:06:18 +0900

    Fix bug of drag

--- lib/src/main/java/jp/kuluna/eventgridview/DraggableEventGridListView.kt
+++ lib/src/main/java/jp/kuluna/eventgridview/DraggableEventGridListView.kt
@@ -44,7 +44,9 @@ class DraggableEventGridListView : FrameLayout {
                 if (it.action == DragEvent.ACTION_DRAG_ENDED) {
                     handler.removeMessageAll()
                 } else if (it.action != DragEvent.ACTION_DRAG_EXITED) {
-                    val frameY = it.y - binding.scrollView.scrollY
+                    val aScale = context.resources.getDimension(R.dimen.a_scale)
+                    val frameY = it.y - binding.scrollView.scrollY - aScale * (eventGridView.scaleFrom
+                            ?: 0)
                     when {
                         frameY < height * 0.1 -> handler.moveToTop()
                         frameY > height * 0.9 -> handler.moveToBottom()
